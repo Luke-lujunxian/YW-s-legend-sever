3abef6970e17b39c64b066c4588995a6fe660872
HONG ZIXIANG <291043595@qq.com>
2018-05-01 11:47:09 +0800

    洪梓翔2018.5.1.
    增加了主要的几个类，并加上了一些skill类里面的基本内容

---
+++ YWLegend/src/YW_Skills.java
@@ -0,0 +1,13 @@
+public class YW_Skills extends Skill {
+    YW_Skills(String new_name, int new_power, String new_description, Character new_owner){
+        super(new_name,new_power,new_description,new_owner);
+    }
+
+    public void BloodPressSkill(){
+    }
+
+    public void StrangeAltarSkill(){
+    }
+
+
+}
