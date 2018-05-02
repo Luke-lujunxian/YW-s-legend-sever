ce0e234362191ad9a21dce32e735ac49e810fb47
Hudayday <32944717+HudayMuhammad@users.noreply.github.com>
2018-05-02 14:10:59 +0800

    May 5.2 YW更新2.0

---
+++ YWLegend/yw.java
@@ -0,0 +1,56 @@
+public class yw {//有待接口化
+    public String name;
+    private String phyle;
+    private String religion;
+    private String saying;
+    private int energy;
+    private int blood;
+    private int attack;
+    private int attackRange;
+    public yw(){
+    }
+    public yw(String yname,String yphyle,String yreligion){
+        name = yname;
+        phyle = yphyle;
+        religion = yreligion;
+            energy = 1;
+            blood = 1;
+            attack = 1;
+            attackRange = 1;
+            //ability 待实现
+            saying = "这是一个尹巍（出现尹巍时请重启游戏并回报bug）";
+    }
+
+    public String getPhyle(){
+        return this.phyle;
+    }
+    public String getReligion(){
+        return this.religion;
+    }
+    public int getBlood(){
+        return this.blood;
+    }
+    public int getEnergy(){
+        return this.energy;
+    }
+    public int getAttack(){
+        return this.attack;
+    }
+    public int getAttackRange(){
+        return this.attackRange;
+    }
+    public String getInfo(){//实际数据投影
+        return name+" ("+energy+"/"+blood+"/"+attack+"/"+attackRange+")";
+    }
+    public String die(){//阵亡
+        return saying;
+    }
+    public void getHurt(int hurt){//受伤
+        blood-=hurt;
+    }
+    public void getRecure(int newBlood,int newAttack){//回复
+        blood+=newBlood;
+        attack+=newAttack;
+    }
+
+}
