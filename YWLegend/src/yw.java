14d80e448017841b002e684b308e6892b234e7c8
HONG ZIXIANG <291043595@qq.com>
2018-05-02 15:15:41 +0800

    1

--- YWLegend/src/yw.java
+++ YWLegend/src/yw.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author lenovo
- */
-public class yw {//鏈夊緟鎺ュ彛鍖�
+public class yw {//有待接口化
     public String name;
     private String phyle;
     private String religion;
@@ -23,13 +13,12 @@ public class yw {//鏈夊緟鎺ュ彛鍖�
         name = yname;
         phyle = yphyle;
         religion = yreligion;
-        if (name=="muslim"){
-            energy = 10;
-            blood = 100;
-            attack = 5;
+            energy = 1;
+            blood = 1;
+            attack = 1;
             attackRange = 1;
-            //ability 寰呭疄鐜�
-            saying = "鎴戠殑涔岀帥锛屾洐鍏夊凡鐜�";}
+            //ability 待实现
+            saying = "这是一个尹巍（出现尹巍时请重启游戏并回报bug）";
     }
 
     public String getPhyle(){
@@ -50,16 +39,16 @@ public class yw {//鏈夊緟鎺ュ彛鍖�
     public int getAttackRange(){
         return this.attackRange;
     }
-    public String getInfo(){//瀹為檯鏁版嵁鎶曞奖
+    public String getInfo(){//实际数据投影
         return name+" ("+energy+"/"+blood+"/"+attack+"/"+attackRange+")";
     }
-    public String die(){//闃典骸
+    public String die(){//阵亡
         return saying;
     }
-    public void getHurt(int hurt){//鍙椾激
+    public void getHurt(int hurt){//受伤
         blood-=hurt;
     }
-    public void getRecure(int newBlood,int newAttack){//鍥炲
+    public void getRecure(int newBlood,int newAttack){//回复
         blood+=newBlood;
         attack+=newAttack;
     }
