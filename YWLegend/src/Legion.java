e9d0f23b8d87f489578779ef770b7c7f603c5edf
Jeff Dui <1174980556@qq.com>
2018-05-09 09:20:34 +0800

    legion军团第一次修改
    还不确定军团数据放数组里还是直接传对象。感觉传对象数据修改而已很同步

--- YWLegend/src/Legion.java
+++ YWLegend/src/Legion.java
@@ -1,3 +1,12 @@
 public class Legion {
+    int[] chara_pos;//计划用数组来储存角色信息和yw信息
+    int[] yw_pos;
+    Legion(yw character, yw b){
+        chara_pos=new int[4];
+        yw_pos=new int[12];
 
+    }
+    public void change_status(){
+
+    }
 }
