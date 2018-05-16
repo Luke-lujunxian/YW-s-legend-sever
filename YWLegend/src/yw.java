d64844821106188b4b7db4960dbaf81fa537d790
HONG ZIXIANG <291043595@qq.com>
2018-05-16 14:17:27 +0800

    skill继续增加

--- YWLegend/src/yw.java
+++ YWLegend/src/yw.java
@@ -1,17 +1,5 @@
 public class yw implements Character{//有待接口化
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-    /*
-        Planning:
-     */
->>>>>>> a9f594b43f41593f6c0e936c49957ab3d085a89a
-=======
-    /*
-        Planning:
-     */
->>>>>>> a9f594b43f41593f6c0e936c49957ab3d085a89a
     private Character character;
     public String name;
     private String phyle;
