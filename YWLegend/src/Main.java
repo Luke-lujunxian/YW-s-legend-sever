0d1417342d1045444dad9bd9cf176c567489bdf4
HONG ZIXIANG <291043595@qq.com>
2018-05-17 11:18:54 +0800

    Main

--- YWLegend/src/Main.java
+++ YWLegend/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
-    public static void main(String[] args){
+
+    public static String beforeGameSelfChecking (String checkingWords) {
+        if(checkingWords=="") return "LJXZDL";
+        return "not good";
+    }
+
+    
+
+    public static String middleTurn () {
+
+        return null;
+    }
+
+
+
+    public static void main (String[] args) {
 
     }
+
 }
