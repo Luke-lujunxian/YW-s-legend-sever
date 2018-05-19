affb8921e438b83cf0378535a033ddf92e004de7
Jeff Dui <1174980556@qq.com>
2018-05-19 11:57:50 +0800

    通讯协议更新：规范数据格式
    创建CommunicationStandard类，通讯时创建该类对象传入字符串数组即可

---
+++ YWLegend/src/CommunicationStandard.java
@@ -0,0 +1,29 @@
+public class CommunicationStandard {
+    String importData;
+    public CommunicationStandard(){
+        importData=null;
+    }
+    public CommunicationStandard(String[] new_importData){
+        outputDataForm(new_importData);
+        outputDataLength(importData);
+    }
+    private void outputDataForm(String[] new_importData){
+        for(int i=0;i<new_importData.length;i++){
+            if(i<new_importData.length-1) importData+=new_importData[i]+"\u00A1";
+            else importData+=new_importData;
+        }
+    }
+    private String outputDataLength(String operated_importData){
+        int lengthTemp=operated_importData.length();
+        String dataLength="";
+        for(int i=0;i<4;i++){
+            if(lengthTemp!=0){
+                dataLength+=(lengthTemp%10);
+                lengthTemp/=10;
+            }else{
+                dataLength+=0;
+            }
+        }
+        return dataLength;
+    }
+}
