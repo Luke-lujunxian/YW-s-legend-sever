c63747748891d2ed5e716dd08223adac6d7c0815
Jeff Dui <1174980556@qq.com>
2018-05-19 20:29:29 +0800

    MessageDeciher架构

---
+++ YWLegend/src/MessageDecipher.java
@@ -0,0 +1,15 @@
+public class MessageDecipher extends SubThread{
+    public MessageDecipher(){
+
+    }
+    public static void decipher(String[] code, Player_1 A,Player_2 B ){
+        if(code[0].equals("ywPlacement")){
+            if(code[1].equals(A.myLegion.))
+        }else if(code[0].equals("Attack")){
+
+        }else if(code[0].equals("Moving")){
+
+        }
+
+    }
+}
