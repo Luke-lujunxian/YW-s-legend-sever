b02740a500c8231b74852fd1130b5be118535ce2
Jeff Dui <1174980556@qq.com>
2018-05-21 19:13:35 +0800

    Unhandled Exception错误更新
    exception没有使用tryc，atch。handled，暂时注释

--- YWLegend/src/Legion.java
+++ YWLegend/src/Legion.java
@@ -76,7 +76,7 @@ public class Legion {
     private void initial_status(Character character,yw placement_1,yw placement_2,yw placement_3,yw placement_4,int pos_x,int pos_y){//初始化信息，欲待完善
         //characters[0]=character;
         if(character instanceof  Personage) leader=(Personage)character;
-        else throw new Exception("noPersoange");
+        //else throw new Exception("noPersoange");
         characters[0]=placement_1;
         placement_1.yw_pos=0;
         characters[1]=placement_2;
